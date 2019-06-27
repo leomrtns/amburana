0c81774a6419b3f6f3f71dc93d5a4f129e88776e
Leonardo de-Oliveira-Martins (QIB) <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2019-06-27 17:53:50 +0100

    skeleton

---
+++ tests/check_unit.c
@@ -0,0 +1,14 @@
+#include <amburana.h> 
+#include <check.h>
+
+#define TEST_SUCCESS 0
+#define TEST_FAILURE 1
+#define TEST_SKIPPED 77
+#define TEST_HARDERROR 99
+
+int
+main ()
+{
+  // if it compiles I am happy 
+  return TEST_SUCCESS; 
+}
