37ef948623a1a8fb5a5915bea374d2ed4b90a2a4
Leonardo de-Oliveira-Martins (QIB) <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2019-07-22 17:14:56 +0100

    speed up aglommerative distance calculation (by storing internal node distance)

--- src/clustering_lancewilliams.c
+++ src/clustering_lancewilliams.c
@@ -4,10 +4,6 @@
  * Implements Agglomerative Hierarchical Clustering algorithm.
  **/
 
-#define NOT_USED  0 /* node is currently not used */
-#define LEAF_NODE 1 /* node contains a leaf node */
-#define A_MERGER  2 /* node contains a merged pair of root clusters */
-
 #define alloc_mem(N, T) (T *) calloc(N, sizeof(T))
 
 typedef struct cluster_s cluster_t;
@@ -22,12 +18,10 @@ struct cluster_s {
 };
 
 struct cluster_node_s {
-  int type; /* type of the cluster node */
-  int is_root; /* true if cluster hasn't merged with another */
-  int height; /* height of node from the bottom */
-  coord_t centroid; /* centroid of this cluster */
-  char *label; /* label of a leaf node */
-  int *merged; /* indexes of root clusters merged */
+  bool is_leaf; /* leaf or merged */ 
+  bool is_root; /* true if cluster hasn't merged with another */
+  double height[2], length; /* height of node from the bottom (= distance) and to lower node */
+  int merged[2]; /* indexes of root clusters merged */
   int n_items; /* number of leaf nodes inside new cluster */
   int *item; /* array of leaf nodes indices inside merged clusters */
   neighbour_t *neighbours; /* sorted linked list of distances to roots */
@@ -35,8 +29,7 @@ struct cluster_node_s {
 
 struct neighbour_s {
   int target; /* the index of cluster node representing neighbour */
-  double distance; /* distance between the nodes */
-  double variance; /* LEO: store info about new cluster (Ward variance for instance)  */
+  double distance[2]; /* distance between the nodes and extra info (variance for Ward's mehod) */
   neighbour_t *next, *prev; /* linked list entries */
 };
 
@@ -53,16 +46,16 @@ agglomerative_cluster_run ()
   get_k_clusters(cluster, k);
 }
 
-double get_distance (agglomerative_cluster ag, int index, int target)
+double get_distance_into_neighbour (agglomerative_cluster ag, neighbour_t neighbour, int index, int target)
 {
   /* if both are leaves, just use the distances matrix */
+  // TODO update distance[2] (not just return) 
   if (index < ag->cluster->num_items && target < ag->cluster->num_items) return distance_generator_get (ag->d, index, target);
   return ag->distance_fptr (ag, a->items, ag->cluster->nodes[index], ag->cluster->nodes[target]);
 }
 
 double single_linkage (agglomerative_cluster ag,  cluster_node_t *a, cluster_node_t *b)
 {
-  // LEO : should receive cluster_node_t (with centroid info etc) instead of numbers
   double min = DBL_MAX, d;
   for (int i = 0; i < a->n_items; ++i) for (int j = 0; j < b->n_items; ++j) {
     d = distance_generator_get (ag->d, a->item[i], b->item[j]);
@@ -98,6 +91,159 @@ double ward_linkage (agglomerative_cluster ag,  cluster_node_t *a, cluster_node_
   return  d / (a->n_items + b->n_items) - a->variance/a->n_items - b->variance/b->n_items;
   // save d somewhere, if accepted we update it to d + a->var + b->var
 }
+void insert_before(neighbour_t *current, neighbour_t *neighbours, cluster_node_t *node)
+{
+  neighbours->next = current;
+  if (current->prev) {
+    current->prev->next = neighbours;
+    neighbours->prev = current->prev;
+  } 
+  else node->neighbours = neighbours;
+  current->prev = neighbours;
+}
+
+void insert_after(neighbour_t *current, neighbour_t *neighbours)
+{
+  neighbours->prev = current;
+  current->next = neighbours;
+}
+
+void insert_sorted(cluster_node_t *node, neighbour_t *neighbours)
+{
+  neighbour_t *temp = node->neighbours;
+  while (temp->next) {
+    if (temp->distance[0] >= neighbours->distance[0]) {
+      insert_before(temp, neighbours, node);
+      return;
+    }
+    temp = temp->next;
+  }
+  if (neighbours->distance[0] < temp->distance[0]) insert_before(temp, neighbours, node);
+  else  insert_after(temp, neighbours);
+}
+
+neighbour_t *add_neighbour(cluster_t *cluster, int index, int target)
+{
+  neighbour_t *neighbour = alloc_mem (1, neighbour_t);
+  neighbour->target = target;
+  neighbour->distance = get_distance_into_neighbour (cluster, neighbour, index, target);  // LEO: only called here
+  cluster_node_t *node = &(cluster->nodes[index]);
+  if (node->neighbours) insert_sorted(node, neighbour);  // LEO: only called here
+  else node->neighbours = neighbour;
+  return neighbour;
+}
+
+cluster_t *update_neighbours(cluster_t *cluster, int index)
+{
+  cluster_node_t *node = &(cluster->nodes[index]);
+  int root_clusters_seen = 1, target = index;
+  while (root_clusters_seen < cluster->num_clusters) {
+    cluster_node_t *temp = &(cluster->nodes[--target]);
+    if (temp->is_root) {
+      ++root_clusters_seen;
+      add_neighbour(cluster, index, target);  // LEO: only called here
+    }
+  }
+  return cluster;
+}
+
+void merge_items (cluster_t *cluster, cluster_node_t *node, int ids[2])
+{
+  int k = 0, idx;
+  
+  node->is_leaf = false;
+  node->is_root = true;
+
+  for (int i = 0; i < 2; ++i) {
+    cluster_node_t *t = &(cluster->nodes[ids[i]]);
+    t->is_root = false; /* no longer root: merged */
+    for (int j = 0; j < t->num_items; ++j) { /* copy leaf indexes from merged clusters */
+      idx = t->items[j];
+      node->items[k++] = idx;
+    }
+    t->length = (node->height[0] - t->height[0]) / 2.; // branch length is half the difference in cluster distances
+  }
+}
+
+cluster_node_t *merge (cluster_t *cluster, int ids[2], double best_distance[2])
+{
+  int new_idx = cluster->num_nodes;
+  cluster_node_t *node = &(cluster->nodes[new_idx]);
+  node->merged[0] = ids[0];
+  node->merged[1] = ids[1];
+  node->height[0] = best_distance[0]; // cluster distance 
+  node->height[1] = best_distance[1]; // extra info (variance in Ward, or avge distance in upgma)
+  node->num_items = cluster->nodes[ids[0]].num_items + cluster->nodes[ids[1]].num_items;
+  node->items = alloc_mem(node->num_items, int);
+  merge_items (cluster, node, ids);
+  cluster->num_nodes++;
+  cluster->num_clusters--;
+  update_neighbours(cluster, node_idx);
+  return node;
+}
+
+cluster_t *agglomerate(int num_items, item_t *items)
+{
+  int ids[2];
+  double best_distance[2] = {DBL_MAX, 0.};
+  cluster_t *cluster = alloc_mem(1, cluster_t);
+  cluster->nodes = alloc_mem(2 * num_items - 1, cluster_node_t);
+  cluster->distances =  generate_distance_matrix(num_items, items);
+  cluster->num_items = num_items;
+  cluster->num_nodes = 0;
+  cluster->num_clusters = 0;
+
+  for (int i = 0; i < cluster->num_items; ++i) if (add_leaf (cluster, &items[i])) update_neighbours(cluster, i); 
+
+  while (cluster->num_clusters > 1) if (find_clusters_to_merge(cluster, ids, best_distance) != -1) merge (cluster, ids, best_distance);
+  return cluster;
+}
+
+cluster_node_t *add_leaf (cluster_t *cluster, const item_t *item)
+{
+  cluster_node_t *leaf = &(cluster->nodes[cluster->num_nodes]);
+  leaf->items = alloc_mem(1, int);
+  node->label_id = cluster->num_nodes; // redundant, same as items[0] 
+  node->is_leaf = node->is_root = true;
+  node->height[0] = node->height[1] = node->length  = 0.;
+  node->num_items = 1;
+  node->items[0] = cluster->num_nodes++;
+  cluster->num_clusters++;
+  return leaf;
+}
+
+int find_clusters_to_merge (cluster_t *cluster, int ids[2], double best_distance[2])
+{
+  int root_clusters_seen = 0;
+  int j = cluster->num_nodes; /* traverse hierarchy top-down */
+  ids[0] = -1;
+  best_distance[0] = DBL_MAX; // maybe zero is fine, due to "-1"
+  while (root_clusters_seen < cluster->num_clusters) {
+    cluster_node_t *node = &(cluster->nodes[--j]);
+    if (!node->is_root)  continue;
+    ++root_clusters_seen;
+    find_best_distance_neighbour (cluster->nodes, j, node->neighbours, ids, best_distance);
+  }
+  return ids[0];
+}
+
+void find_best_distance_neighbour (cluster_node_t *nodes, int node_idx, neighbour_t *neighbour, int ids[2], double best_distance[2])
+{
+  while (neighbour) {
+    if (nodes[neighbour->target].is_root) {
+      if (ids[0] == -1 || neighbour->distance[0] < best_distance[0]) {
+        ids[0] = node_idx;
+        ids[1] = neighbour->target;
+        best_distance[0] = neighbour->distance[0];
+        best_distance[1] = neighbour->distance[1];
+      }
+      break;
+    }
+    neighbour = neighbour->next;
+  }
+}
+
+
 
 void free_neighbours (neighbour_t *node)
 {
@@ -129,266 +275,11 @@ void free_cluster(cluster_t * cluster)
   }
 }
 
-void insert_before(neighbour_t *current, neighbour_t *neighbours, cluster_node_t *node)
-{
-  neighbours->next = current;
-  if (current->prev) {
-    current->prev->next = neighbours;
-    neighbours->prev = current->prev;
-  } 
-  else node->neighbours = neighbours;
-  current->prev = neighbours;
-}
-
-void insert_after(neighbour_t *current, neighbour_t *neighbours)
-{
-  neighbours->prev = current;
-  current->next = neighbours;
-}
-
-void insert_sorted(cluster_node_t *node, neighbour_t *neighbours)
-{
-  neighbour_t *temp = node->neighbours;
-  while (temp->next) {
-    if (temp->distance >= neighbours->distance) {
-      insert_before(temp, neighbours, node);
-      return;
-    }
-    temp = temp->next;
-  }
-  if (neighbours->distance < temp->distance)  insert_before(temp, neighbours, node);
-  else  insert_after(temp, neighbours);
-}
-
-neighbour_t *add_neighbour(cluster_t *cluster, int index, int target)
-{
-  neighbour_t *neighbour = alloc_mem(1, neighbour_t);
-  if (neighbour) {
-    neighbour->target = target;
-    neighbour->distance = get_distance(cluster, index, target);
-    cluster_node_t *node = &(cluster->nodes[index]);
-    if (node->neighbours) insert_sorted(node, neighbour);
-    else node->neighbours = neighbour;
-  } else alloc_fail("neighbour node");
-  return neighbour;
-}
-
-cluster_t *update_neighbours(cluster_t *cluster, int index)
-{
-  cluster_node_t *node = &(cluster->nodes[index]);
-  if (node->type == NOT_USED) {
-    invalid_node(index);
-    cluster = NULL;
-  } else {
-    int root_clusters_seen = 1, target = index;
-    while (root_clusters_seen < cluster->num_clusters) {
-      cluster_node_t *temp = &(cluster->nodes[--target]);
-      if (temp->type == NOT_USED) {
-        invalid_node(index);
-        cluster = NULL;
-        break;
-      }
-      if (temp->is_root) {
-        ++root_clusters_seen;
-        add_neighbour(cluster, index, target);
-      }
-    }
-  }
-  return cluster;
-}
-
-#define init_leaf(cluster, node, item, len)             \
-  do {                                            \
-    strncpy(node->label, item->label, len); \
-    node->centroid = item->coord;           \
-    node->type = LEAF_NODE;                 \
-    node->is_root = 1;                      \
-    node->height = 0;                       \
-    node->num_items = 1;                    \
-    node->items[0] = cluster->num_nodes++;  \
-  } while (0)                                     \
-
-cluster_node_t *add_leaf(cluster_t *cluster, const item_t *item)
-{
-  cluster_node_t *leaf = &(cluster->nodes[cluster->num_nodes]);
-  int len = strlen(item->label) + 1;
-  leaf->label = alloc_mem(len, char);
-  if (leaf->label) {
-    leaf->items = alloc_mem(1, int);
-    if (leaf->items) {
-      init_leaf(cluster, leaf, item, len);
-      cluster->num_clusters++;
-    } else {
-      alloc_fail("node items");
-      free(leaf->label);
-      leaf = NULL;
-    }
-  } else {
-    alloc_fail("node label");
-    leaf = NULL;
-  }
-  return leaf;
-}
-
-#undef init_leaf
-
-cluster_t *add_leaves(cluster_t *cluster, item_t *items)
-{
-  for (int i = 0; i < cluster->num_items; ++i) {
-    if (add_leaf(cluster, &items[i])) update_neighbours(cluster, i);
-    else { cluster = NULL; break; }
-  }
-  return cluster;
-}
-
-void merge_items(cluster_t *cluster, cluster_node_t *node, cluster_node_t **to_merge)
-{
-  node->type = A_MERGER;
-  node->is_root = 1;
-  node->height = -1;
-  // LEO: main function to modify (replace centroid by ward's avge distance^2 between points etc) 
-  /* copy leaf indexes from merged clusters */
-  int k = 0, idx;
-  coord_t centroid = { .x = 0.0, .y = 0.0 };
-  for (int i = 0; i < 2; ++i) { 
-    cluster_node_t *t = to_merge[i];
-    t->is_root = 0; /* no longer root: merged */
-    if (node->height == -1 || node->height < t->height) node->height = t->height;
-    for (int j = 0; j < t->num_items; ++j) {
-      idx = t->items[j];
-      node->items[k++] = idx;
-    }
-    centroid.x += t->num_items * t->centroid.x;
-    centroid.y += t->num_items * t->centroid.y;
-  }
-  /* calculate centroid */
-  node->centroid.x = centroid.x / k;
-  node->centroid.y = centroid.y / k;
-  node->height++;
-}
-
-#define merge_to_one(cluster, to_merge, node, node_idx)         \
-  do {                                                    \
-    node->num_items = to_merge[0]->num_items +      \
-    to_merge[1]->num_items;                 \
-    node->items = alloc_mem(node->num_items, int);  \
-    if (node->items) {                              \
-      merge_items(cluster, node, to_merge);   \
-      cluster->num_nodes++;                   \
-      cluster->num_clusters--;                \
-      update_neighbours(cluster, node_idx);   \
-    } else {                                        \
-      alloc_fail("array of merged items");    \
-      free(node->merged);                     \
-      node = NULL;                            \
-    }                                               \
-  } while(0)                                              \
-
-cluster_node_t *merge(cluster_t *cluster, int first, int second)
-{
-  int new_idx = cluster->num_nodes;
-  cluster_node_t *node = &(cluster->nodes[new_idx]);
-  node->merged = alloc_mem(2, int);
-  if (node->merged) {
-    cluster_node_t *to_merge[2] = {
-      &(cluster->nodes[first]),
-      &(cluster->nodes[second])
-    };
-    node->merged[0] = first;
-    node->merged[1] = second;
-    merge_to_one(cluster, to_merge, node, new_idx);
-  } else {
-    alloc_fail("array of merged nodes");
-    node = NULL;
-  }
-  return node;
-}
-
-#undef merge_to_one
-
-void find_best_distance_neighbour(cluster_node_t *nodes, int node_idx, neighbour_t *neighbour, double *best_distance, int *first, int *second)
-{
-  while (neighbour) {
-    if (nodes[neighbour->target].is_root) {
-      if (*first == -1 || neighbour->distance < *best_distance) {
-        *first = node_idx;
-        *second = neighbour->target;
-        *best_distance = neighbour->distance;
-      }
-      break;
-    }
-    neighbour = neighbour->next;
-  }
-}
-
-
-int find_clusters_to_merge(cluster_t *cluster, int *first, int *second)
-{
-  double best_distance = 0.0;
-  int root_clusters_seen = 0;
-  int j = cluster->num_nodes; /* traverse hierarchy top-down */
-  *first = -1;
-  while (root_clusters_seen < cluster->num_clusters) {
-    cluster_node_t *node = &(cluster->nodes[--j]);
-    if (node->type == NOT_USED || !node->is_root)  continue;
-    ++root_clusters_seen;
-    find_best_distance_neighbour(cluster->nodes, j, node->neighbours, &best_distance, first, second);
-  }
-  return *first;
-}
-
-cluster_t *merge_clusters(cluster_t *cluster)
-{
-  int first, second;
-  while (cluster->num_clusters > 1) {
-    if (find_clusters_to_merge(cluster, &first, &second) != -1) merge(cluster, first, second);
-  }
-  return cluster;
-}
-
-#define init_cluster(cluster, num_items, items)                         \
-  do {                                                            \
-    cluster->distances =                                    \
-    generate_distance_matrix(num_items, items);     \
-    if (!cluster->distances)                                \
-    goto cleanup;                                   \
-    cluster->num_items = num_items;                         \
-    cluster->num_nodes = 0;                                 \
-    cluster->num_clusters = 0;                              \
-    if (add_leaves(cluster, items))                         \
-    merge_clusters(cluster);                        \
-    else                                                    \
-    goto cleanup;                                   \
-  } while (0)                                                     \
-
-cluster_t *agglomerate(int num_items, item_t *items)
-{
-  cluster_t *cluster = alloc_mem(1, cluster_t);
-  if (cluster) {
-    cluster->nodes = alloc_mem(2 * num_items - 1, cluster_node_t);
-    if (cluster->nodes) init_cluster(cluster, num_items, items);
-    else {
-      alloc_fail("cluster nodes");
-      goto cleanup;
-    }
-  } else alloc_fail("cluster");
-  goto done;
-
-cleanup:
-  free_cluster(cluster);
-  cluster = NULL;
-
-done:
-  return cluster;
-}
-
-#undef init_cluster
-
 int print_root_children(cluster_t *cluster, int i, int nodes_to_discard)
 {
   cluster_node_t *node = &(cluster->nodes[i]);
   int roots_found = 0;
-  if (node->type == A_MERGER) {
+  if (!node->is_leaf) {
     for (int j = 0; j < 2; ++j) {
       int t = node->merged[j];
       if (t < nodes_to_discard) {
@@ -431,8 +322,8 @@ void print_cluster_items(cluster_t *cluster, int index)
   cluster_node_t *node = &(cluster->nodes[index]);
   fprintf(stdout, "Items: ");
   if (node->num_items > 0) {
-    fprintf(stdout, "%s", cluster->nodes[node->items[0]].label);
-    for (int i = 1; i < node->num_items; ++i) fprintf(stdout, ", %s", cluster->nodes[node->items[i]].label);
+    fprintf(stdout, "%3d", cluster->nodes[node->items[0]].label_id);
+    for (int i = 1; i < node->num_items; ++i) fprintf(stdout, ", %3d", cluster->nodes[node->items[i]].label_id);
   }
   fprintf(stdout, "\n");
 }
@@ -441,7 +332,7 @@ void print_cluster_node(cluster_t *cluster, int index)
 {
   cluster_node_t *node = &(cluster->nodes[index]); 
   fprintf(stdout, "Node %d - height: %d, centroid: (%5.3f, %5.3f)\n", index, node->height, node->centroid.x, node->centroid.y);
-  if (node->label) fprintf(stdout, "\tLeaf: %s\n\t", node->label);
+  if (node->is_leaf) fprintf(stdout, "\tLeaf: %3d\n\t", node->label_id);
   else fprintf(stdout, "\tMerged: %d, %d\n\t", node->merged[0], node->merged[1]);
   print_cluster_items(cluster, index);
   fprintf(stdout, "\tNeighbours: ");
